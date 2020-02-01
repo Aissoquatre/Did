a4cd6406b264a73a062b2d00d76d1f238520a625
Julien Bernard <hello@julien-bernard.com>
2020-02-01 21:32:53 +0100

    Add first vers of update scripts

--- src/Database/Update.php
+++ src/Database/Update.php
@@ -10,13 +10,50 @@ namespace Did\Database;
  */
 class Update extends AbstractConnection
 {
-    public function __construct()
+    /**
+     * @var string
+     */
+    private $path;
+
+    /**
+     * Update constructor.
+     * @param string $path
+     */
+    public function __construct($path)
     {
         parent::connect();
+
+        $this->path = $path;
     }
 
     public function run()
     {
-        return true;
+        $files = array_diff(scandir($this->path), ['..', '.']);
+
+        foreach ($files as $file) {
+            $fileFullPath = $this->path . DIRECTORY_SEPARATOR . $file;
+
+            if (!is_file($fileFullPath)) {
+                continue;
+            }
+
+            $extension = pathinfo($file, PATHINFO_EXTENSION);
+
+            if ($extension === 'php') {
+
+            } elseif ($extension === 'sql') {
+                $content = explode(';', file_get_contents($fileFullPath));
+
+                foreach ($content as $query) {
+                    if (empty($query)) {
+                        continue;
+                    }
+
+                    $this->db->exec($query);
+                }
+            }
+        }
+
+        return 'OK';
     }
 }
